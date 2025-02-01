245e145f3d8698e93f05d1b22b3fea55eb2faa36
rodolphodgo <rodolphogouveiageo@gmail.com>
2025-01-31 22:25:26 -0300

    exercicios de logica

--- LogicaComPython/Aula03Funcoes/Exercicio07.py
+++ LogicaComPython/Aula03Funcoes/Exercicio07.py
@@ -5,3 +5,11 @@
 3   3   3
 .....
 n   n   n   n   n   n  ... n"""
+
+def piramide(n):
+    for x in range(1, n + 1):
+        for y in range(x):
+            print(x, end=" ")
+        print()
+
+piramide(5)
--- LogicaComPython/Aula03Funcoes/Exercicio08.py
+++ LogicaComPython/Aula03Funcoes/Exercicio08.py
@@ -5,3 +5,11 @@
 1   2   3
 .....
 1   2   3   ...  n"""
+
+def piramide(n):
+    for x in range(1, n + 1):
+        for y in range(1, x + 1):
+            print(y, end=" ")
+        print()
+
+piramide(5)
