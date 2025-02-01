697bfeacd3f3df100384602ffbb0d8e46f390eb6
rodolphodgo <rodolphogouveiageo@gmail.com>
2025-01-31 21:42:18 -0300

    exercicios de LP com Python

--- LogicaComPython/Aula03Funcoes/Exercicio10.py
+++ LogicaComPython/Aula03Funcoes/Exercicio10.py
@@ -1,1 +1,6 @@
 """Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o imposto sobre vendas."""
+
+def somaImposto(taxaImposto, custo):
+    print(((taxaImposto * custo) / 100) + custo)
+
+somaImposto(35, 500)
--- LogicaComPython/Aula03Funcoes/Exercicio11.py
+++ LogicaComPython/Aula03Funcoes/Exercicio11.py
@@ -1,3 +1,13 @@
 """Um palíndromo é uma palavra que se soletra da mesma forma nos dois sentidos, como “osso” e “reviver”. Escreva um função que dado uma plavra verifique se ela é palindro.
 
 """
+
+def palidromo(palavra):
+    if palavra == palavra[::-1]:
+        print("É um palidromo")
+    else:
+        print("Não é um palidromo")
+
+palidromo("osso")
+palidromo("reviver")
+palidromo("palavra")
