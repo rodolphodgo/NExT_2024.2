3e63bf86c0ee34891cb5402b94a127e346db0bbb
rodolphodgo <rodolphogouveiageo@gmail.com>
2025-02-03 21:08:21 -0300

    Exercício de lógica

--- LogicaComPython/Aula04ProgramacaoFuncional/Exercicio07.py
+++ LogicaComPython/Aula04ProgramacaoFuncional/Exercicio07.py
@@ -1,6 +1,15 @@
-'''Maior palavra em uma lista
+"""Maior palavra em uma lista
 
-Utilize reduce para encontrar a palavra mais longa na lista palavras = ["sol", "montanha", "mar", "universo"].'''
+Utilize reduce para encontrar a palavra mais longa na lista palavras = ["sol", "montanha", "mar", "universo"]."""
 
 from functools import reduce
+
+lista = ["sol", "montanha", "mar", "universo"]
+
+maior_palavra = reduce(lambda x, y: x if len(x) > len(y) else y, lista)
+print(maior_palavra)
+
+
+
+
 
